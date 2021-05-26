486b419413c16d702c2ccfd84de5531144689566
siyeon oh <osiyun0313@gmail.com>
2021-05-26 19:44:57 +0900

    syoh update(2)

--- routes/teacher.js
+++ routes/teacher.js
@@ -6,8 +6,9 @@ const { checkTokenTeacher } = require('../middlewares/auth');
 router.get('/ping', teacher.ping);
 router.post('/', teacher.createTeacher);
 router.post('/login', teacher.loginTeacher);
-router.get('/list', checkToken, teacher.teachers);
-router.get('/', checkToken, teacher.getTeacherInfo);
-router.put('/', checkToken, teacher.editTeacherInfo);
+router.get('/list', checkTokenTeacher, teacher.teachers);
+router.get('/', checkTokenTeacher, teacher.getTeacherInfo);
+router.put('/', checkTokenTeacher, teacher.editTeacherInfo);
+router.get('/', checkTokenTeacher, teacher.searchStudent)
 
 module.exports = router;
